eb998c4cfdda2ed8b84d3017b2ce9145448ed4bb
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-04-17 14:23:25 +0900

    ジェネレーター制作中

--- Assets/Adachi/Scripts/EnemyGenerator.cs
+++ Assets/Adachi/Scripts/EnemyGenerator.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    List<GameObject> _enemy = new List<GameObject>();
+    /// <summary></summary>
+    [SerializeField,Header("生成するもの")] List<GameObject> _object = new List<GameObject>();
+    /// <summary>オブジェクトの番号</summary>
+    int _objectNumber;
+    /// <summary>オブジェクトを生成する時間間隔</summary>
+    float _intervalTime;
+    /// <summary>オブジェクトを生成する位置</summary>
+    Vector3 _pos;
 
     void Start()
     {
@@ -19,7 +26,13 @@ public class EnemyGenerator : MonoBehaviour
 
     IEnumerator Generator()
     {
-        
-        yield return new WaitForSeconds(0f);
+        while(true)
+        {
+            _objectNumber = Random.Range(0, _object.Count);
+            _pos = new Vector3(Random.Range(0f, 5f), Random.Range(0f, 5f), 0f);
+            _intervalTime = Random.Range(0.1f,3f);
+            Instantiate(_object[0],_pos, Quaternion.identity);
+            yield return new WaitForSeconds(_intervalTime);
+        }       
     }
 }
