29ac09828d51e104d4a05834e529805a42dff593
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-04-17 13:56:45 +0900

    wip

---
+++ Assets/Adachi/Scripts/EnemyGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    List<GameObject> _enemy = new List<GameObject>();
+
+    void Start()
+    {
+        StartCoroutine(Generator());
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator Generator()
+    {
+        
+        yield return new WaitForSeconds(0f);
+    }
+}
