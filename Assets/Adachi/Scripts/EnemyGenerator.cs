7278a3592503a734b9114a886309754244a54b97
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-04-17 15:05:53 +0900

    ジェネレーター制作中

--- Assets/Adachi/Scripts/EnemyGenerator.cs
+++ Assets/Adachi/Scripts/EnemyGenerator.cs
@@ -4,34 +4,53 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    /// <summary></summary>
+    /// <summary>生成するもの</summary>
     [SerializeField,Header("生成するもの")] List<GameObject> _object = new List<GameObject>();
     /// <summary>オブジェクトの番号</summary>
     int _objectNumber;
-    /// <summary>オブジェクトを生成する時間間隔</summary>
+    /// <summary>開始時間</summary>
+    [SerializeField,Header("開始時間")]float _beginTime = 5f;
+    /// <summary>オブジェクト生成の時間間隔</summary>
     float _intervalTime;
+    /// <summary>オブジェクト生成の最短時間</summary>
+    [SerializeField,Header("オブジェクト生成の最短時間")]float _maximumTime = 5f;
+    /// <summary>オブジェクト生成の最長時間</summary>
+    [SerializeField, Header("オブジェクト生成の最長時間")] float _minimumTime = 0.1f;
+    /// <summary>上限（ｙの限界）</summary>
+    [SerializeField,Header("上限（yの限界）")]float _upperLimit = 4f;
+    /// <summary>下限（-ｙの限界）</summary>
+    [SerializeField, Header("下限（-ｙの限界）")] float _downLimit = -4f;
+    /// <summary>右限（xの限界）</summary>
+    [SerializeField, Header("右限（xの限界）")] float _rightLimit = 10f;
+    /// <summary>左限（-xの限界）</summary>
+    [SerializeField, Header("左限（-xの限界）")] float _leftLimit = 10f;
     /// <summary>オブジェクトを生成する位置</summary>
     Vector3 _pos;
+    /// <summary>判定回数の制御</summary>
+    const float _JUDGMENTTIME = 1 / 60;
 
     void Start()
     {
         StartCoroutine(Generator());
     }
 
-    void Update()
-    {
-        
-    }
-
-
     IEnumerator Generator()
     {
+        yield return new WaitForSeconds(_beginTime);
+        Debug.Log("Start!");
         while(true)
         {
+            //判定回数の制御
+            yield return new WaitForSeconds(_JUDGMENTTIME);
+            //生成するオブジェクトをランダムで決める
             _objectNumber = Random.Range(0, _object.Count);
-            _pos = new Vector3(Random.Range(0f, 5f), Random.Range(0f, 5f), 0f);
-            _intervalTime = Random.Range(0.1f,3f);
-            Instantiate(_object[0],_pos, Quaternion.identity);
+            //生成する座標をランダムで決める
+            _pos = new Vector3(Random.Range(_leftLimit, _rightLimit), Random.Range(_downLimit, _upperLimit), 0f);
+            //生成頻度をランダムで決める
+            _intervalTime = Random.Range(_minimumTime,_maximumTime);
+            //生成する
+            Instantiate(_object[_objectNumber],_pos, Quaternion.identity);
+            //オブジェクト生成の時間間隔
             yield return new WaitForSeconds(_intervalTime);
         }       
     }
---
+++ Assets/Adachi/Scripts/HumanMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMove : MonoBehaviour
+{
+    /// <summary>剛体</summary>
+    Rigidbody2D _rb;
+    /// <summary>人のスピード</summary>
+    [SerializeField, Header("")] float _speed;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = Vector2.left;   
+    }
+}
