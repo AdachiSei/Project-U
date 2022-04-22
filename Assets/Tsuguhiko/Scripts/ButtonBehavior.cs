c4f54b740e6fa2d066a13747928b3a7278ef37ac
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-23 01:40:25 +0900

    ボタン動作のスクリプトを追加。

---
+++ Assets/Tsuguhiko/Scripts/ButtonBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+/// <summary>ステージセレクト画面でのボタン動作スクリプト</summary>
+
+public class ButtonBehavior : MonoBehaviour
+{
+    [SerializeField, Header("ボタン各種")] Button[] _buttons;
+
+    [SerializeField,Header("Touch操作イベントシステム")] EventSystem _eventSystem;
+
+    [SerializeField, Header("シーン切り換え待機時間")] float _second;
+
+    [SerializeField, Header("遷移シーン名")] string _sceneName;
+
+    WaitForSeconds _wfs;
+
+
+    void Awake()
+    {
+        _wfs = new WaitForSeconds(_second);
+    }
+
+    public void LoadScene()
+    {
+        StartCoroutine(StandByTime());
+    }
+
+    IEnumerator StandByTime()
+    {
+        _eventSystem.enabled = false;
+
+        yield return _wfs;
+
+        SceneManager.LoadScene(_sceneName);
+
+        _eventSystem.enabled = true;
+    }
+}
