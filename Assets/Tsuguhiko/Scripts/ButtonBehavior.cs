445fde1e04ba8afc02b51ec1a8f761b1d558c769
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-24 08:10:03 +0900

    ButtonBehaviorスクリプトにコメントを追加。

--- Assets/Tsuguhiko/Scripts/ButtonBehavior.cs
+++ Assets/Tsuguhiko/Scripts/ButtonBehavior.cs
@@ -9,17 +9,22 @@ using UnityEngine.EventSystems;
 
 public class ButtonBehavior : MonoBehaviour
 {
+    /// <summary>それぞれのボタンを設定する配列のメンバ変数</summary>
     [SerializeField, Header("ボタン各種")] Button[] _buttons;
 
+    /// <summary>各デバイスのボタン操作のオンオフを切り換えるメンバ変数(このゲームではスマートフォン操作)</summary>
     [SerializeField,Header("Touch操作イベントシステム")] EventSystem _eventSystem;
 
+    /// <summary>WaitforSecondsでの待機時間のメンバ変数</summary>
     [SerializeField, Header("シーン切り換え待機時間")] float _second;
 
+    /// <summary>移動させるシーン名を設定するメンバ変数</summary>
     [SerializeField, Header("遷移シーン名")] string _sceneName;
 
+    /// <summary>WaitforSecondsのメンバ変数</summary>
     WaitForSeconds _wfs;
 
-
+    /// <summary>再生した時にWaitforSecondsをキャッシュさせるためのAwakeメソッド(メモリ削減のため)</summary>
     void Awake()
     {
         _wfs = new WaitForSeconds(_second);
