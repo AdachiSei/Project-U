b3a9853dec3eb163b7be7c81b2b5f8cc471dd44f
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-04-28 23:30:38 +0900

    GameManager.csを編集した。

--- Assets/Tsuguhiko/Scripts/GameManager.cs
+++ Assets/Tsuguhiko/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>メインゲームの処理を管理するスクリプト</summary>
+
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
